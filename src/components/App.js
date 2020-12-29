b4b53982e004d055258fd64eb65c41b981770c42
DhanushM12 <dhanushmukhi@gmail.com>
2020-12-29 20:44:38 +0530

    imported components in app

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
+import React, { Component } from 'react'
+import Header from './Header'
+import Taskbar from './Taskbar'
+import Tasklist from './Tasklist'
 
-
-function App() {
-  return (
-    <div>
-     Hello
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Taskbar />
+        <Tasklist />
+      </div>
+    )
+  }
 }
 
-export default App;
