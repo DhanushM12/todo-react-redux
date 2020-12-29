5b54889171ae88ae8943d86919c5a3459217b274
DhanushM12 <dhanushmukhi@gmail.com>
2020-12-29 21:08:36 +0530

    updated the taskbar

--- src/components/Taskbar.js
+++ src/components/Taskbar.js
@@ -1,12 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {addTask} from '../actions/index';
 
-export default class Taskbar extends Component {
+class Taskbar extends Component {
     render() {
         return (
             <div>
                 <input type='text' ref='task' placeholder='add task'/>
-                <button>Add Task</button>
+                <button onClick={()=> this.props.addTask(this.refs.task.value)}>Add Task</button>
             </div>
         )
     }
 }
+
+function mapDispatchToProps(dispatch){
+    return bindActionCreators({addTask}, dispatch);
+}
+export default connect(()=>{}, mapDispatchToProps)(Taskbar);
